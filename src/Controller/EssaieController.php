fee923d98cc664279745ca624f59240b40da07d2
noe <noegouton@gmail.com>
2024-01-25 14:00:33 +0100

    suites 1 template mail

--- src/Controller/EssaieController.php
+++ src/Controller/EssaieController.php
@@ -11,11 +11,12 @@ class EssaieController extends AbstractController
     #[Route('/essaie', name: 'app_essaie')]
     public function index(): Response
     {
-        // return $this->render('emails/verif_mail.html.twig', [
-        // return $this->render('emails/passe_4got_mail.html.twig', [
-        // return $this->render('emails/pass_edit_mail.html.twig', [
-        return $this->render('emails/bienvenu_mail.html.twig', [
+       // return $this->render('emails/verif_mail.html.twig', [
+         return $this->render('emails/passe_4got_mail.html.twig', [
+       //  return $this->render('emails/pass_edit_mail.html.twig', [
+      //  return $this->render('emails/bienvenu_mail.html.twig', [
             'controller_name' => 'EssaieController',
+         //   'speudo' => 'EssaieController',
         ]);
     }
 }
