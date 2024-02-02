cea8d010411d4151f2ac97554c748174c940a78c
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-02-02 15:47:19 +0100

    depo des informations dans les champs

--- src/Controller/AdminInterfaceController.php
+++ src/Controller/AdminInterfaceController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Services\TraitementsDS;
+use App\Repository\EnvRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminInterfaceController extends AbstractController
 {
+    private $em;
+    private $env;
+    private $traitementsDS;
+
+    public function __construct(EntityManagerInterface $em, EnvRepository $env, TraitementsDS $traitementsDS)
+    {
+        $this->em = $em;
+        $this->env = $env->find(1);
+        $this->traitementsDS = $traitementsDS;
+    } 
     #[Route('/admin/interface', name: 'app_admin_interface')]
     public function index(): Response
     {
         return $this->render('admin_interface/index.html.twig', [
-            'controller_name' => 'AdminInterfaceController',
+            'env' => $this->env,
         ]);
     }
 }
