36895f6be8ebcf10ef81759514f7a8ff165b9aa1
noe <noegouton@gmail.com>
2024-02-02 15:09:08 +0100

    interface_admin_debut

---
+++ src/Controller/AdminInterfaceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminInterfaceController extends AbstractController
+{
+    #[Route('/admin/interface', name: 'app_admin_interface')]
+    public function index(): Response
+    {
+        return $this->render('admin_interface/index.html.twig', [
+            'controller_name' => 'AdminInterfaceController',
+        ]);
+    }
+}
