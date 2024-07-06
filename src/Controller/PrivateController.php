37cafeecf70f2d699073112154a9e269099d049f
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-07-06 09:17:09 +0100

    depos

--- src/Controller/PrivateController.php
+++ src/Controller/PrivateController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Controller\API\UserController;
+use App\Repository\EnvRepository;
 use App\Repository\UserRepository;
 use App\Services\CookieDS;
 use App\Services\SessionDS;
 use App\Services\TraitementsDS;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +16,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PrivateController extends AbstractController
 {
+    private $em;
+    private $env;
+    private $cookieDS;
+    private $traitementsDS;
+    private $userRepository;
+
+    public function __construct(EntityManagerInterface $em, EnvRepository $env, UserRepository $userRepository, CookieDS $cookieDS, TraitementsDS $traitementsDS)
+    {
+        $this->em = $em;
+        $this->env = $env->find(1);
+        $this->cookieDS = $cookieDS;
+        $this->traitementsDS = $traitementsDS;
+        $this->userRepository = $userRepository;
+    }
+
+    function getUserByUidInCookies(){
+        if($this->cookieDS->get("uid")){
+            $uid = $this->cookieDS->get("uid");
+            $user = $this->userRepository->findOneBy(['uid' => $uid]);
+            if($user){
+                return $user;
+            }
+        }
+        return false;
+    }
+
     #[Route('/logout', name: 'app_logout')]
     public function logout(CookieDS $cookieDS, UserRepository $userRepository, TraitementsDS $traitementsDS): Response
     {
@@ -38,8 +67,10 @@ class PrivateController extends AbstractController
     #[Route('/actu', name: 'app_actu')]
     public function actu(): Response
     {
+        $user = $this->getUserByUidInCookies();
+        $userinfo = $this->traitementsDS->infosUser($user);
         $html = $this->renderView('private/actu.html.twig', [
-            'controller_name' => 'PrivateController',
+            'actus' => json_decode($userinfo['lesPublicites']),
         ]);
 
         return new JsonResponse([
