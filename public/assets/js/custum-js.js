33c350f2f3c5c93302b2e54617329c71f22f8a0b
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-07-15 18:32:01 +0100

    confirmation mail et numero

--- public/assets/js/custum-js.js
+++ public/assets/js/custum-js.js
@@ -1634,6 +1634,115 @@ $(document).ready(function () {
         });
     });
 
+    $(document).on("click", "#envoyerCodeMail", function () {
+        traitementContact("envoyerCodeMail", "debut", "")
+        let uid = $("#uid").val();
+        $.ajax({
+            type: "POST",
+            url: "/api/sendMailVerification",
+            data: {
+                uid : uid,
+                langUserPhone : 'fr',
+            },
+            success: function (response) {
+                if(response.error == true){
+                    $("#envoyerCodeMail").removeClass("btn-primary").addClass("btn-danger").text("ERREUR").attr("disabled", "");
+                } else {
+                    $("#envoyerCodeMail").removeClass("btn-primary").addClass("btn-success").text("Code Envoyer").attr("disabled", "");
+                }
+            }
+        });
+    });
+
+    $(document).on("click", "#validerVerifMail", function () {
+        traitementContact("validerVerifMail", "debut", "")
+
+        let msgError = "Veuillez renseigner :"
+        let msgErrorHtml = $("#msgErrorValideMail").text()
+
+        let uid = $("#uid").val();
+        let codeVerifMail = $("#codeVerifMail").val();
+
+        $(".getInfoVerifMail").each(function() {
+            let titre = $(this).prev().text();
+            if(!titre){ titre = $(this).attr("placeholder"); }
+            let value = $(this).val();
+            if(!value){ 
+                if(msgError == "Veuillez renseigner :") {
+                    msgError += " " + titre
+                } else {
+                    msgError += ", " + titre
+                }
+            }
+        });
+
+        if(msgError != "Veuillez renseigner :"){
+            if(!msgErrorHtml){
+                $("#msgErrorValideMail").html(`
+                    <div class="alert border-0 border-danger border-start border-4 bg-light-danger alert-dismissible fade show py-2">
+                    <div class="d-flex align-items-center">
+                    <div class="fs-3 text-danger"><i class="bi bi-x-circle-fill"></i>
+                    </div>
+                    <div class="ms-3">
+                        <div class="text-danger">`+msgError+`</div>
+                    </div>
+                    </div>
+                    </div>
+                `);
+                $("#msgErrorValideMail").toggle(800)
+            }
+            traitementContact("validerVerifMail", "fin", "Valider")
+            return 0;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "/api/mailVerification",
+            data: {
+                uid : uid,
+                langUserPhone : 'fr',
+                codeForVerifMail : codeVerifMail,
+            },
+            success: function (response) {
+                if(response.error == true){
+                    $("#msgErrorValideMail").html(`
+                        <div class="alert border-0 border-danger border-start border-4 bg-light-danger alert-dismissible fade show py-2">
+                        <div class="d-flex align-items-center">
+                        <div class="fs-3 text-danger"><i class="bi bi-x-circle-fill"></i>
+                        </div>
+                        <div class="ms-3">
+                            <div class="text-danger">`+response.message+`</div>
+                        </div>
+                        </div>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    `);
+                    $("#msgErrorValideMail").toggle(800)
+                } else {
+                    msgError = "C'est Valider..."
+                    $("#msgErrorValideMail").html(`
+                        <div class="alert border-0 border-success border-start border-4 bg-light-success alert-dismissible fade show py-2">
+                        <div class="d-flex align-items-center">
+                        <div class="fs-3 text-success"><i class="bi bi-x-circle-fill"></i>
+                        </div>
+                        <div class="ms-3">
+                            <div class="text-success">`+msgError+`</div>
+                        </div>
+                        </div>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    `);
+                    $(".getInfo").val("");
+                    $("#msgErrorValideMail").toggle(800);
+                    setTimeout(() => {
+                        window.location.reload()
+                    }, 2000);
+                }
+                traitementContact("validerVerifMail", "fin", "Valider")
+            }
+        });
+    });
+
     /**
      * Clique sur un element du menu
      */
