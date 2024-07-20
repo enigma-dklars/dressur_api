dcb7b39975bc775499f5944d0a7ec2ff8e380a0b
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-07-20 08:43:31 +0100

    MAJ

--- src/Controller/Crud/CrudCampagneMailController.php
+++ src/Controller/Crud/CrudCampagneMailController.php
@@ -56,9 +56,9 @@ class CrudCampagneMailController extends AbstractController
         ini_set("memory_limit", "-1");
 
         if(rand(0, 100000) % 2 == 0) {
-            $cent_mails_pending = $fileAttenteCampagneMailRepository->findBy([], ['id' => 'ASC'], 25);
+            $cent_mails_pending = $fileAttenteCampagneMailRepository->findBy([], ['id' => 'ASC'], 20);
         } else {
-            $cent_mails_pending = $fileAttenteCampagneMailRepository->findBy([], ['id' => 'DESC'], 25);
+            $cent_mails_pending = $fileAttenteCampagneMailRepository->findBy([], ['id' => 'DESC'], 20);
         }
         foreach ($cent_mails_pending as $un_mail) {
             try {
