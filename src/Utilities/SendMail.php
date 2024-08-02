ff0d547b92270fb546e2eccc07dcfc0c4f86005e
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-08-02 13:54:41 +0100

    les mails fonctionnel

--- src/Utilities/SendMail.php
+++ src/Utilities/SendMail.php
@@ -19,25 +19,17 @@ class SendMail {
         // ["noreply3@dressur.site", "nunewqi_DS3", "smtp.titan.email", "465", "ssl"],
         // ["noreply4@dressur.site", "nunewqi_DS3", "smtp.titan.email", "465", "ssl"],
         // ["noreply5@dressur.site", "nunewqi_DS3", "smtp.titan.email", "465", "ssl"],
+        
         ["noreply1.dressur.ds@gmail.com", "maatzpxlfnunewqi"],
         ["noreply2.dressur.ds@gmail.com", "eeufhrlrlbicxdmr"],
         ["noreply3.dressur.ds@gmail.com", "styzjjntshtyhzcr"],
         ["noreply4.dressur.ds@gmail.com", "vutvoskfzywhpgjp"],
-        ["noreply5.dressur.ds@gmail.com", "znjaliibcopwqgea"],
+
         ["noreply6.dressur.ds@gmail.com", "uxfzdpsgqkbzryhy"],
         ["noreply7.dressur.ds@gmail.com", "fkfpukqzwlgueadu"],
         ["noreply8.dressur.ds@gmail.com", "gsdmjxfqnzjbwpxq"],
         ["noreply9.dressur.ds@gmail.com", "amjybgcikdvtsrem"],
         ["noreply10.dressur.ds@gmail.com", "ylmdtwreacsymamc"],
-
-        ["noreply11.dressur.ds@gmail.com", "rlbsslizifaozwvx"],
-        ["noreply12.dressur.ds@gmail.com", "lqcndnuyhlszgpxa"],
-        ["noreply13.dressur.ds@gmail.com", "gksmtwsrsanndorr"],
-        ["noreply14.dressur.ds@gmail.com", "htzremgpiyfvkfdi"],
-        // ["noreply15.dressur.ds@gmail.com", "puolmrjnisqxpkhz"],
-        // ["noreply16.dressur.ds@gmail.com", "rwyslcjbdzfwlbyx"],
-        // ["noreply17.dressur.ds@gmail.com", "ygxrqmdjlbmmcsey"],
-        // ["dressurun@aol.com", "Fhp*k8M%&r%3QnS"],
     ];
 
     public function __construct(LoggerInterface $logger) {
