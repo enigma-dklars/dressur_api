d19cea090c5c4119fd214e110e58ac9e47ab1b28
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-08-20 10:02:54 +0100

    crud EnvPaiementApi

---
+++ src/Controller/Crud/CrudEnvPaiementApiController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller\Crud;
+
+use App\Services\CookieDS;
+use App\Entity\EnvPaiementApi;
+use App\Services\TraitementsDS;
+use App\Form\EnvPaiementApiType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\EnvPaiementApiRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/crud/env/paiement/api')]
+class CrudEnvPaiementApiController extends AbstractController
+{
+    private $theme;
+    private $cookieDS;
+    private $traitementsDS;
+
+    public function __construct(CookieDS $cookieDS, TraitementsDS $traitementsDS)
+    {
+        $this->cookieDS = $cookieDS;
+        $this->traitementsDS = $traitementsDS;
+        if($this->cookieDS->check("theme")) {
+            if($this->cookieDS->get("theme") == "dark-theme") {
+                $this->theme = "dark-theme";
+            } else {
+                $this->theme = "light-theme";
+            }
+        } else {
+            $this->theme = "light-theme";
+        }
+    }
+    
+    #[Route('/', name: 'app_env_paiement_api_index', methods: ['GET'])]
+    public function index(EnvPaiementApiRepository $envPaiementApiRepository): Response
+    {
+        return $this->render('crud_env_paiement_api/index.html.twig', [
+            'theme' => $this->theme,
+            'user' => $this->traitementsDS->getUserByUidInCookies(),
+            'env_paiement_apis' => $envPaiementApiRepository->findBy([], ['id' => 'DESC']),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_env_paiement_api_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $envPaiementApi = new EnvPaiementApi();
+        $form = $this->createForm(EnvPaiementApiType::class, $envPaiementApi);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($envPaiementApi);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_env_paiement_api_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('crud_env_paiement_api/new.html.twig', [
+            'theme' => $this->theme,
+            'user' => $this->traitementsDS->getUserByUidInCookies(),
+            'env_paiement_api' => $envPaiementApi,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_env_paiement_api_show', methods: ['GET'])]
+    public function show(EnvPaiementApi $envPaiementApi): Response
+    {
+        return $this->render('crud_env_paiement_api/show.html.twig', [
+            'theme' => $this->theme,
+            'user' => $this->traitementsDS->getUserByUidInCookies(),
+            'env_paiement_api' => $envPaiementApi,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_env_paiement_api_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EnvPaiementApi $envPaiementApi, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EnvPaiementApiType::class, $envPaiementApi);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_env_paiement_api_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('crud_env_paiement_api/edit.html.twig', [
+            'theme' => $this->theme,
+            'user' => $this->traitementsDS->getUserByUidInCookies(),
+            'env_paiement_api' => $envPaiementApi,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_env_paiement_api_delete', methods: ['POST'])]
+    public function delete(Request $request, EnvPaiementApi $envPaiementApi, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$envPaiementApi->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($envPaiementApi);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_env_paiement_api_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/EnvPaiementApi.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EnvPaiementApiRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EnvPaiementApiRepository::class)]
+class EnvPaiementApi
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $aggregator = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $apiKey = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $environment = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $endpointSecret = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $routeWebhook = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $countTransactionApproved = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $activated = null;
+
+    public function __construct()
+    {
+        $this->activated = false;
+        $this->environment = "live";
+        $this->aggregator = "FedaPay";
+        $this->countTransactionApproved = 0;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAggregator(): ?string
+    {
+        return $this->aggregator;
+    }
+
+    public function setAggregator(?string $aggregator): static
+    {
+        $this->aggregator = $aggregator;
+
+        return $this;
+    }
+
+    public function getApiKey(): ?string
+    {
+        return $this->apiKey;
+    }
+
+    public function setApiKey(?string $apiKey): static
+    {
+        $this->apiKey = $apiKey;
+
+        return $this;
+    }
+
+    public function getEnvironment(): ?string
+    {
+        return $this->environment;
+    }
+
+    public function setEnvironment(?string $environment): static
+    {
+        $this->environment = $environment;
+
+        return $this;
+    }
+
+    public function getEndpointSecret(): ?string
+    {
+        return $this->endpointSecret;
+    }
+
+    public function setEndpointSecret(?string $endpointSecret): static
+    {
+        $this->endpointSecret = $endpointSecret;
+
+        return $this;
+    }
+
+    public function getRouteWebhook(): ?string
+    {
+        return $this->routeWebhook;
+    }
+
+    public function setRouteWebhook(?string $routeWebhook): static
+    {
+        $this->routeWebhook = $routeWebhook;
+
+        return $this;
+    }
+
+    public function getCountTransactionApproved(): ?int
+    {
+        return $this->countTransactionApproved;
+    }
+
+    public function setCountTransactionApproved(?int $countTransactionApproved): static
+    {
+        $this->countTransactionApproved = $countTransactionApproved;
+
+        return $this;
+    }
+
+    public function isActivated(): ?bool
+    {
+        return $this->activated;
+    }
+
+    public function setActivated(?bool $activated): static
+    {
+        $this->activated = $activated;
+
+        return $this;
+    }
+}
