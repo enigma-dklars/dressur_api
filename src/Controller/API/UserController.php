5189d26a13e1ec0b2ec7da0b643ce473b49d2395
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-09-03 12:12:25 +0100

    recallé les adresses et tel bannis lors de la modification du profil

--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -438,6 +438,36 @@ class UserController extends AbstractController
 
         $uid = $datas->get('uid');
 
+        if(in_array($tel, $this->env->getUserBanned())) {
+            if($sessionDS->get("langUserPhone") != "fr") {
+                return new JsonResponse([
+                    'error' => true,
+                    'titre' => 'Mistake!',
+                    'message' => "This number has been banned from Dressur. Contact support if this is an error.",
+                ]);
+            }
+            return new JsonResponse([
+                'error' => true,
+                'titre' => 'Erreur!',
+                'message' => "Ce numéro a été banni de Dressur. Contacter l'assistance s'il s'agit d'une erreur.",
+            ]);
+        }
+
+        if(in_array($mail, $this->env->getUserBanned())) {
+            if($sessionDS->get("langUserPhone") != "fr") {
+                return new JsonResponse([
+                    'error' => true,
+                    'titre' => 'Mistake!',
+                    'message' => "This email address has been banned from Dressur. Contact support if this is a mistake.",
+                ]);
+            }
+            return new JsonResponse([
+                'error' => true,
+                'titre' => 'Erreur!',
+                'message' => "Cette adresse mail a été banni de Dressur. Contactez l'assistance s'il s'agit d'une erreur.",
+            ]);
+        }
+
         if($instagram) {
             if (!$verificationsDS->isValidSocialUrl($instagram, 'instagram')) {
                 if($sessionDS->get("langUserPhone") != "fr") {
