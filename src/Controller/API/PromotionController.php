905173cc3616173520064826fdf271f266da49e9
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-10-28 14:34:47 +0100

    MAJ API /addProduitService
    if($mode == "oui") { $mode = true; }
            if($mode == "no") { $mode = false; }

--- src/Controller/API/PromotionController.php
+++ src/Controller/API/PromotionController.php
@@ -256,6 +256,8 @@ class PromotionController extends AbstractController
         $uid = $datas->get('uid');
         $text = $datas->get('text');
         $mode = $datas->get('mode');
+        if($mode == "oui") { $mode = true; }
+        if($mode == "no") { $mode = false; }
         $paymentMethod = $datas->get('paymentMethod');
         $tel = $datas->get('tel');
 
