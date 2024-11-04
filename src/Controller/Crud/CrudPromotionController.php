606ef3491a894edb9949e2f1bdf76cc69ef78afb
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-11-04 15:18:19 +0100

    try catch sur la suppression de l'image

--- src/Controller/Crud/CrudPromotionController.php
+++ src/Controller/Crud/CrudPromotionController.php
@@ -189,7 +189,11 @@ class CrudPromotionController extends AbstractController
     {
         if ($this->isCsrfTokenValid('delete'.$promotion->getId(), $request->request->get('_token'))) {
             if(str_starts_with($promotion->getImage(), 'dressur_pro_')) {
-                unlink($this->getParameter('promotion_directory')."/".$promotion->getImage());
+                try {
+                    unlink($this->getParameter('promotion_directory')."/".$promotion->getImage());
+                } catch (\Throwable $th) {
+                    //throw $th;
+                }
             }
             $entityManager->remove($promotion);
             $entityManager->flush();
