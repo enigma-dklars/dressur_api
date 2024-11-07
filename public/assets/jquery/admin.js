b707a84497a1addceb65535a1732058b5a66046e
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-11-07 21:14:42 +0100

    2 suppression des routes admin inutiles

--- public/assets/jquery/admin.js
+++ public/assets/jquery/admin.js
@@ -1,27 +1,4 @@
 
 $(document).ready(function () {
-    $("#envButtonOK").on("click", function () {
-        $(this).attr("disabled", "");
-        let commissionBonus = $("#commissionBonus").val();
-        let versionApp = $("#versionApp").val();
-        let importantUpdate = $("#importantUpdate").val();
-        let doBoostPayant = $("#doBoostPayant").val();
-        let linkLocalServer = $("#linkLocalServer").val();
-
-        $.ajax({
-            type: "POST",
-            url: "/admin/interface/upadte_env_value",
-            data: {
-                commissionBonus:commissionBonus,
-                versionApp:versionApp,
-                importantUpdate:importantUpdate,
-                doBoostPayant:doBoostPayant,
-                linkLocalServer:linkLocalServer,
-            },
-            error: function (response) {
-               alert("ERREUR");
-            }
-        });
-        $(this).removeAttr("disabled");
-    });
+    
 });
