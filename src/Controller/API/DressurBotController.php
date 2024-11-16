f3470be1065b37821681ef6080a008dd4c8f624d
Sarlas ALLADAYE ~ DESKTOP PDG BLT <dklars.dev@gmail.com>
2024-11-16 17:44:01 +0100

    dressur_bot_activation OK

--- src/Controller/API/DressurBotController.php
+++ src/Controller/API/DressurBotController.php
@@ -375,7 +375,74 @@ class DressurBotController extends AbstractController
                 ]);
             }
         } else {
-            // logique de paiement FeexPay
+            // logique fait de paiement FeexPay
+            $envPaiementApi = $traitementsDS->getEnvPaiementApiFeexPayDisponible();
+            if(!$envPaiementApi) {
+                $this->sendMail->sendReport("user bot tel : ".$tel, "Aucun Webhook Disponible pour FeexPay");
+                if($sessionDS->get("langUserPhone") != "fr") {
+                    return new JsonResponse([
+                        'error' => true,
+                        'titre' => 'Erreur!',
+                        'message' => "Payment error. Please contact the administrators.",
+                    ]);
+                }
+                return new JsonResponse([
+                    'error' => true,
+                    'titre' => 'Erreur!',
+                    'message' => "Erreur de paiement. Veuillez contacter les administrateurs SVP.",
+                ]);
+            }
+            
+            try {
+                $resultat = $traitementsDS->startPaiementFeexPay(
+                    $envPaiementApi, 
+                    $methodePaiementEntity, 
+                    $formulDressurBot->getPrix(),
+                    $tel,
+                    $userBotFind->getNom(),
+                    $userBotFind->getEmail(),
+                    "dressur_bot_activation",
+                    [
+                        'userBotId' => $userBotFind->getId(),
+                        'formulDressurBotId' => $formulDressurBot->getId(),
+                    ],
+                    $userBotFind
+                );
+                return new JsonResponse($resultat);
+            } catch (\Throwable $th) {
+                $msgError = (string)$th;
+                if (strpos($msgError, "Vous avez excédé le nombre de transactions hebdomadaire requis. 10 transactions approuvées sont autorisées par semaine.") !== false) {
+                    $envPaiementApi->setCountTransactionApproved(10);
+                    $this->em->flush();
+
+                    if($sessionDS->get("langUserPhone") != "fr") {
+                        return new JsonResponse([
+                            'error' => true,
+                            'titre' => 'Excuse us please!',
+                            'message' => "Please submit the form again. Thank you.",
+                        ]);
+                    }
+                    return new JsonResponse([
+                        'error' => true,
+                        'titre' => 'Excusez-nous svp!',
+                        'message' => "Veuillez soumettre une nouvelle fois le formulaire. Merci.",
+                    ]);
+                }
+
+                $this->sendMail->sendReport("DressurBot uUid : ".$userBotFind->getId()." WhatsApp : ".$userBotFind->getNumero(), $th);
+                if($sessionDS->get("langUserPhone") != "fr") {
+                    return new JsonResponse([
+                        'error' => true,
+                        'titre' => 'Erreur!',
+                        'message' => "We encountered an error. You will be contacted by an administrator.",
+                    ]);
+                }
+                return new JsonResponse([
+                    'error' => true,
+                    'titre' => 'Erreur!',
+                    'message' => "Nous avons rencontré une erreur. Vous serez contacté par un administrateur.",
+                ]);
+            }
         }
 
         return new JsonResponse([
