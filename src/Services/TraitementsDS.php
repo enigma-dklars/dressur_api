6f7051288f47eb0590006b679d2db78d2a1fb03f
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2026-02-13 11:28:31 +0100

    ...

--- src/Services/TraitementsDS.php
+++ src/Services/TraitementsDS.php
@@ -33,14 +33,12 @@ use App\Repository\DSBonusHistoriqueRepository;
 use App\Repository\FormuleDressurBotRepository;
 use App\Controller\API\UserPreferenceController;
 use App\Entity\FormulePromoReseau;
-use App\Entity\MethodePaiement;
 use App\Entity\Transaction;
 use App\Repository\FormulePromoAffaireRepository;
 use App\Repository\FormulePromoReseauRepository;
 use App\Repository\HistoriqueProgrammeRecompenseRepository;
 use App\Repository\MethodePaiementRepository;
 use Feexpay\FeexpayPhp\FeexpayClass;
-use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TraitementsDS extends AbstractController
