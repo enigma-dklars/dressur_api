05bf4b0ebd2aee8831a5123ea8a6f4c162fa60c5
Noreply99 <58599921-noreply991@users.noreply.replit.com>
2026-08-12 16:28:30 +0000

    feat(api): add user public networks endpoints

--- src/Repository/UserSocialNetworkRepository.php
+++ src/Repository/UserSocialNetworkRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\User;
 use App\Entity\UserSocialNetwork;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,5 +15,21 @@ class UserSocialNetworkRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, UserSocialNetwork::class);
+    }
+
+    /**
+     * @return list<UserSocialNetwork>
+     */
+    public function findForUser(User $user): array
+    {
+        return $this->findBy(['user' => $user], ['id' => 'ASC']);
+    }
+
+    public function findOneForUser(User $user, string $networkType): ?UserSocialNetwork
+    {
+        return $this->findOneBy([
+            'user' => $user,
+            'networkType' => $networkType,
+        ]);
     }
 }
